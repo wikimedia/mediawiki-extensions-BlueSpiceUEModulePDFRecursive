837691f11e30e1d2b1c6a7dedef3d11df1660409
pwirth <wirth@hallowelt.biz>
2020-01-17 12:47:37 +0100

    Skip invalid Titles and provide text when Title does not exists
    ERM:17720
    [3.1.x]
    
    Change-Id: I216573fd1149f9193758bc4c8640b8107aee8fa2
    (cherry picked from commit 2905de4ade4552a81459bffb585bb8605ded824f)

--- UEModulePDFRecursive.class.php
+++ UEModulePDFRecursive.class.php
@@ -147,7 +147,7 @@ class UEModulePDFRecursive extends BsExtensionMW {
 			}
 
 			$title = Title::newFromText( $linkTitle );
-			if ( $title == null ) {
+			if ( $title == null || !$title->canExist() ) {
 				continue;
 			}
 
@@ -161,8 +161,14 @@ class UEModulePDFRecursive extends BsExtensionMW {
 			}
 
 			$pageProvider = new BsPDFPageProvider();
-			$pageProviderContent = $pageProvider->getPage( [ 'article-id' => $title->getArticleID() ] );
+			$pageProviderContent = $pageProvider->getPage( [
+				'article-id' => $title->getArticleID(),
+				'title' => $title->getFullText()
+			] );
 
+			if ( !isset( $pageProviderContent['dom'] ) ) {
+				continue;
+			}
 			$DOMDocument = $pageProviderContent['dom'];
 
 			$documentLinks = $DOMDocument->getElementsByTagName( 'a' );
