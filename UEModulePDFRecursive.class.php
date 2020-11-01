eb03bebc18b4403d24574851a5cb885d23b00bc8
dvogel <daniel.vogel@hallowelt.com>
2020-11-01 14:15:50 +0100

    Fix jumpmarks in PDF
    ERM18587
    
    Change-Id: I52f193854ba183bed7e503d53b01d2e66fb74dc4

--- UEModulePDFRecursive.class.php
+++ UEModulePDFRecursive.class.php
@@ -237,6 +237,13 @@ class UEModulePDFRecursive extends BsExtensionMW {
 			$linkTitle = $anchor->getAttribute( 'data-bs-title' );
 			if ( $linkTitle ) {
 				$pathBasename = str_replace( '_', ' ', $linkTitle );
+				$href  = $anchor->getAttribute( 'href' );
+
+				$parsedHref = parse_url( $href );
+				$linkMap[$pathBasename] = md5( $pathBasename );
+				if ( isset( $parsedHref['fragment'] ) ) {
+					$linkMap[$pathBasename] = md5( $pathBasename ) . '-' . md5( $parsedHref['fragment'] );
+				}
 			} else {
 				$href = $anchor->getAttribute( 'href' );
 
@@ -271,7 +278,6 @@ class UEModulePDFRecursive extends BsExtensionMW {
 			if ( !isset( $linkMap[$pathBasename ] ) ) {
 				continue;
 			}
-
 			$anchor->setAttribute( 'href', '#' . $linkMap[$pathBasename] );
 		}
 	}
